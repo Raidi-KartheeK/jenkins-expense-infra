23e7deeefedfc3d70c742250232402c74bb876a4
Your Name <you@example.com>
2024-12-23 20:25:08 +0530

    msg

--- 10-sg/provider.tf
+++ 10-sg/provider.tf
@@ -8,7 +8,7 @@ terraform {
   
   backend "s3" {
     bucket = "aws-remote-state-dev"
-    key    = "expense-sg-dev"
+    key    = "expense-sg"
     region = "us-east-1"
     dynamodb_table = "aws-locking-dev"
   }
